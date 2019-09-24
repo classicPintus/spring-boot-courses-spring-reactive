f2361bab5044f2cf0ea1982c7e7e5013bfafa21c
Gavi <g.pintus@widegroup.ch>
2019-09-24 15:05:43 +0200

    Add endpoint che non parla col db

--- src/main/java/com/codeway/rest/controller/PostRestControllerImpl.java
+++ src/main/java/com/codeway/rest/controller/PostRestControllerImpl.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class PostRestControllerImpl implements PostRestController {
 
@@ -32,6 +36,17 @@ public class PostRestControllerImpl implements PostRestController {
     @GetMapping(path = "/posts-without-mapping/{identifier}")
     public ResponseEntity<PostDomain> getPostWithoutMapping(@PathVariable("identifier") String identifier) {
         return ResponseEntity.ok(postPersistencePort.findByIdentifier(identifier));
+    }
+
+    @GetMapping(path = "/posts-only-memory")
+    public ResponseEntity<Void> getPostOnlyMemory() {
+        Map<String, Instant> m = new HashMap<>();
+        for (int i = 0; i < 100000; i++){
+            int value = 10000 - i + i * 2;
+            String key = String.valueOf(value);
+            m.put(key, Instant.now());
+        }
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping(path = "/posts")
