72ed9a8d00c4f7492f91080221cf41d3601d0a16
Gavi <g.pintus@widegroup.ch>
2019-09-25 11:53:59 +0200

    Add endpoint che non parla col db

--- src/main/java/com/codeway/rest/controller/PostRestControllerImpl.java
+++ src/main/java/com/codeway/rest/controller/PostRestControllerImpl.java
@@ -7,14 +7,18 @@ import com.codeway.persistence.repository.PostDocumentRepository;
 import com.codeway.rest.mapper.PostMapper;
 import com.codeway.rest.model.PostModel;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
@@ -53,13 +57,30 @@ public class PostRestControllerImpl implements PostRestController {
     @GetMapping(path = "/posts-only-memory")
     public ResponseEntity<Mono<Void>> getPostOnlyMemory() {
         Map<String, Instant> m = new HashMap<>();
-        for (int i = 0; i < 100000; i++){
+        for (int i = 0; i < 100000; i++) {
             int value = 10000 - i + i * new Random().nextInt();
             String key = String.valueOf(value);
             m.put(key, Instant.now());
         }
         return ResponseEntity.ok(Mono.empty());
     }
+
+    @GetMapping(path = "/posts-http")
+    public ResponseEntity<Mono<String>> getPostHttp() {
+        Mono<String> stringFlux = WebClient.create()
+                .get()
+                .uri(URI.create("http://192.168.245.157:8080/api/auth/slow"))
+                .retrieve()
+                .bodyToMono(String.class);
+
+        return ResponseEntity.ok(stringFlux);
+    }
+
+    @GetMapping(path = "/posts-http-blocking")
+    public ResponseEntity<String> getPostHttpBlocking() {
+        return ResponseEntity.ok(new RestTemplate().getForObject(URI.create("http://192.168.245.157:8080/api/auth/slow"), String.class));
+    }
+
 
     @PostMapping(path = "/posts")
     @Override
